ead61490953c20dab7b05eb5d6d33ce7808912d6
Mateusz <mateuszs1803@gmail.com>
2022-07-16 19:26:48 +0200

    Extracting Data from Wikipedia

--- wikipedia_scraping.py
+++ wikipedia_scraping.py
@@ -3,19 +3,23 @@
 import requests
 import wikipedia
 
+print("START")
 pages = 500
 titles = [wikipedia.random(1) for i in range(pages)]
-
+new_list = []
 print(titles)
-
-new_list = "'https://de.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=en&titles=" + titles + "'"
+for i in titles:
+    new_list = new_list.append("'https://de.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=en&titles=" + titles + "'")
 
 print(new_list)
 
-response = requests.get('https://de.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=en&titles=Türkei')
+# response = requests.get('https://de.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=en&titles=Türkei')
 
 # print(response.text)
 
-json_api = response.json()
+# json_api = response.json()
 
 #print(json_api["query"]["pages"]["9145"]["langlinks"][0]["*"])
+
+
+print("END")
