b6e32ce2185442848d8789060cb160e7a595d71e
Mateusz <mateuszs1803@gmail.com>
2022-07-28 20:12:09 +0200

    fjkfd

--- WikipediaTesting.py
+++ WikipediaTesting.py
@@ -4,6 +4,7 @@ import requests
 
 
 
-request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=de&titles=Companion%20dog")
+# request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=de&titles=Companion%20dog")
 
-print(request.summary())
+# request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&titles=Albert%20Einstein")
+
