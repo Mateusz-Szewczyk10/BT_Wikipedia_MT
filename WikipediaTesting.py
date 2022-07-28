ef9c01ac663bb809e9aa0787054e18b3600455ca
Mateusz <mateuszs1803@gmail.com>
2022-07-28 20:12:42 +0200

    dklds

--- WikipediaTesting.py
+++ WikipediaTesting.py
@@ -6,5 +6,6 @@ import requests
 
 # request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=de&titles=Companion%20dog")
 
-# request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&titles=Albert%20Einstein")
+request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&titles=Albert%20Einstein")
 
+print(request.json())
