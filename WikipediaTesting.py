f571873b211aaf219249ee2084791457481bd053
Mateusz <mateuszs1803@gmail.com>
2022-07-28 20:14:51 +0200

    k

--- WikipediaTesting.py
+++ WikipediaTesting.py
@@ -6,6 +6,6 @@ import requests
 
 # request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllang=de&titles=Companion%20dog")
 
-request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&titles=Albert+Einstein&format=json")
+request = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&titles=Albert+Einstein&format=json&lllang=de")
 
 print(request.json())
